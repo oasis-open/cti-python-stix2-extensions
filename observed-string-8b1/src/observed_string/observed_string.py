b5829a031ac68dfb0711eeb034dae75cc3e9c7fc
Rich Piazza <rpiazza@mitre.org>
2025-04-10 15:42:01 -0400

    fix observed_string, again

--- observed-string-8b1/src/observed_string/observed_string.py
+++ observed-string-8b1/src/observed_string/observed_string.py
@@ -10,9 +10,8 @@ OBSERVED_STRING_EXTENSION_ID = "extension-definition--8b1aa84c-5532-4c69-a8e7-b6
     'observed-string', [
         # required fields
         ('purpose', OpenVocabProperty(STRING_PURPOSE, required=True)),
-        ('value', StringProperty(required=True)),
-        ('extension_type', StringProperty(fixed=None)),  # needed to indicate it's a sub object extension
+        ('value', StringProperty(required=True))
     ], ['purpose', 'value'], OBSERVED_STRING_EXTENSION_ID,
 )
 class ObservedString:
-    extension_type = 'new-sco'
+    pass
